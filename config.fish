48324b050658285ccf2f4db6214870c6fdfa4277
TheBlackWolf88 <mopszi88@gmail.com>
2025-03-18 13:51:13 +0100

    init

---
+++ config.fish
@@ -0,0 +1,39 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_prompt
+    if test "$(fish_vcs_prompt)" = ""
+        set prompt (prompt_login) '> '
+    else
+        set prompt (prompt_login) '|'(fish_vcs_prompt) '> '
+    end
+    echo $prompt
+end
+
+function fish_right_prompt -d "Write out the right prompt"
+    echo '<'$PWD'>'
+end
+
+set -U fish_greeting
+
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
+
+set -g __fish_git_prompt_color_branch magenta 
+set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_char_upstream_ahead "↑"
+set -g __fish_git_prompt_char_upstream_behind "↓"
+set -g __fish_git_prompt_char_upstream_prefix ""
+
+set -g __fish_git_prompt_char_stagedstate "●"
+set -g __fish_git_prompt_char_dirtystate "✚"
+set -g __fish_git_prompt_char_untrackedfiles "…"
+set -g __fish_git_prompt_char_conflictedstate "✖"
+set -g __fish_git_prompt_char_cleanstate "✔"
+
+set -g __fish_git_prompt_color_dirtystate blue
+set -g __fish_git_prompt_color_stagedstate yellow
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_cleanstate green 
